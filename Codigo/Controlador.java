15c6a37df51113c4a30f7f8bc376747a76824e38
Fabiancipher <fabiancipher@gmail.com>
2025-09-04 21:25:35 -0500

    Deberia funcionar

--- Codigo/Controlador.java
+++ Codigo/Controlador.java
@@ -1,6 +1,9 @@
+import java.util.Random;
 
 public class Controlador{
     private Candidato candidatos[];
+    private Long comparaciones=(long) 0;
+    private Long cambios=(long)0;
 
     public Controlador(int n, int m){
         candidatos = new Candidato[n];
@@ -16,65 +19,215 @@ public class Controlador{
             System.out.println("--------------------------------------------------");
         }
     }
-    public void burbuja(){
-        Candidato aux;
-        for(int i=0; i<candidatos.length-1; i++){
-            for(int j=0; j<candidatos.length-i-1; j++){
-                if(candidatos[j].getTotalSobornos() < candidatos[j+1].getTotalSobornos()){
-                    aux = candidatos[j];
-                    candidatos[j] = candidatos[j+1];
-                    candidatos[j+1] = aux;
-                }
+
+public Candidato[] getCandidatos(){
+    return this.candidatos;
+}
+
+public void estado(Candidato[] arr, int atributo, int estado){
+    switch(estado){
+        case 1:
+            mergeSort(arr, atributo);
+            invertir(arr);
+            break;
+        case 2:
+            mergeSort(arr, atributo);
+            desordenar(arr);
+        default:
+            break;
+    }
+}
+
+private void invertir(Candidato[] arr) {
+    int i = 0, j = arr.length - 1;
+    while (i < j) {
+        swap(arr, i, j);
+        i++;
+        j--;
+    }
+}
+
+private void desordenar(Candidato[] arr) {
+    Random rand = new Random();
+    for (int k = 0; k < arr.length / 10; k++) { // desordena ~10% de elementos
+        int i = rand.nextInt(arr.length);
+        int j = rand.nextInt(arr.length);
+        swap(arr, i, j);
+    }
+}
+
+// Método para comparar según atributo
+private boolean mayorQue(Candidato a, Candidato b, int atributo) {
+    comparaciones++;
+    switch (atributo) {
+        case 1: return a.getTotalMarchas() > b.getTotalMarchas();
+        case 2: return a.getTotalBloqueos() > b.getTotalBloqueos();
+        case 3: return a.getTotalPoliticos() > b.getTotalPoliticos();
+        case 4: return a.getTotalPoliticos() > b.getTotalPrebendas();
+        case 5: return a.getTotalSobornos() > b.getTotalSobornos();
+        default: return false;
+    }
+}
+
+// Intercambio
+private void swap(Candidato[] arr, int i, int j) {
+    cambios++;
+    Candidato temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+}
+
+// ================== BURBUJA ==================
+public void burbuja(Candidato[] arr, int atributo) {
+    comparaciones=(long)0;
+    cambios=(long)0;
+    long inicio = System.nanoTime();
+    int n = arr.length;
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (mayorQue(arr[j], arr[j+1], atributo)) {
+                swap(arr, j, j+1);
             }
         }
     }
-    public void seleccion(){
-        Candidato aux;
-        int pos;
-        for(int i=0; i<candidatos.length-1; i++){
-            pos = i;
-            for(int j=i+1; j<candidatos.length; j++){
-                if(candidatos[j].getTotalSobornos() > candidatos[pos].getTotalSobornos()){
-                    pos = j;
-                }
-            }
-            if(pos != i){
-                aux = candidatos[i];
-                candidatos[i] = candidatos[pos];
-                candidatos[pos] = aux;
+    long fin = System.nanoTime();
+    long tiempo = fin-inicio;
+    System.err.println("Tiempo total de ejecución: "+tiempo+" nanosegundos \n");
+    System.err.println("Comparaciones: "+comparaciones+"\n");
+    System.out.println("Intercambios: "+cambios+"\n");
+}
+
+// ================== INSERCIÓN ==================
+public void insercion(Candidato[] arr, int atributo) {
+    comparaciones=(long)0;
+    cambios=(long)0;
+    long inicio = System.nanoTime();
+    int n = arr.length;
+    for (int i = 1; i < n; i++) {
+        Candidato key = arr[i];
+        int j = i - 1;
+        while (j >= 0 && mayorQue(arr[j], key, atributo)) {
+            arr[j+1] = arr[j];
+            j--;
+        }
+        arr[j+1] = key;
+    }
+    long fin = System.nanoTime();
+    long tiempo = fin-inicio;
+    System.err.println("Tiempo total de ejecución: "+tiempo+" nanosegundos \n");
+    System.err.println("Comparaciones: "+comparaciones+"\n");
+    System.out.println("Intercambios: "+cambios+"\n");
+}
+
+// ================== SELECCIÓN ==================
+public void seleccion(Candidato[] arr, int atributo) {
+    comparaciones=(long)0;
+    cambios=(long)0;
+    long inicio = System.nanoTime();
+    int n = arr.length;
+    for (int i = 0; i < n-1; i++) {
+        int minIdx = i;
+        for (int j = i+1; j < n; j++) {
+            if (mayorQue(arr[minIdx], arr[j], atributo)) {
+                minIdx = j;
             }
         }
+        swap(arr, i, minIdx);
     }
-    public void insercion(){
-        Candidato aux;
-        int j;
-        for(int i=1; i<candidatos.length; i++){
-            aux = candidatos[i];
-            j = i-1;
-            while(j>=0 && candidatos[j].getTotalSobornos() < aux.getTotalSobornos()){
-                candidatos[j+1] = candidatos[j];
-                j--;
-            }
-            candidatos[j+1] = aux;
+    long fin = System.nanoTime();
+    long tiempo = fin-inicio;
+    System.err.println("Tiempo total de ejecución: "+tiempo+" nanosegundos \n");
+    System.err.println("Comparaciones: "+comparaciones+"\n");
+    System.out.println("Intercambios: "+cambios+"\n");
+}
+
+// ================== QUICKSORT ==================
+public void quickSort(Candidato[] arr, int atributo) {
+    comparaciones=(long)0;
+    cambios=(long)0;
+    long inicio = System.nanoTime();
+    quickSortRec(arr, 0, arr.length-1, atributo);
+    long fin = System.nanoTime();
+    long tiempo = fin-inicio;
+    System.err.println("Tiempo total de ejecución: "+tiempo+" nanosegundos \n");
+    System.err.println("Comparaciones: "+comparaciones+"\n");
+    System.out.println("Intercambios: "+cambios+"\n");
+}
+
+private void quickSortRec(Candidato[] arr, int low, int high, int atributo) {
+    if (low < high) {
+        int pi = partition(arr, low, high, atributo);
+        quickSortRec(arr, low, pi-1, atributo);
+        quickSortRec(arr, pi+1, high, atributo);
+    }
+}
+
+private int partition(Candidato[] arr, int low, int high, int atributo) {
+    Candidato pivot = arr[high];
+    int i = low-1;
+    for (int j = low; j < high; j++) {
+        if (mayorQue(pivot, arr[j], atributo)) {
+            i++;
+            swap(arr, i, j);
         }
     }
-    public void quicksort(int izq, int der){
-        Candidato pivote = candidatos[izq];
-        int i = izq;
-        int j = der;
-        Candidato aux;
-        while(i<j){
-            while(candidatos[i].getTotalSobornos() >= pivote.getTotalSobornos() && i<j) i++;
-            while(candidatos[j].getTotalSobornos() < pivote.getTotalSobornos()) j--;
-            if(i<j){
-                aux = candidatos[i];
-                candidatos[i] = candidatos[j];
-                candidatos[j] = aux;
-            }
-        }
-        candidatos[izq] = candidatos[j];
-        candidatos[j] = pivote;
-        if(izq<j-1) quicksort(izq,j-1);
-        if(j+1<der) quicksort(j+1,der);
+    swap(arr, i+1, high);
+    return i+1;
+}
+
+// ================== MERGESORT ==================
+public void mergeSort(Candidato[] arr, int atributo) {
+    comparaciones=(long)0;
+    cambios=(long)0;
+    long inicio = System.nanoTime();
+    mergeSortRec(arr, 0, arr.length-1, atributo);
+    long fin = System.nanoTime();
+    long tiempo = fin-inicio;
+    System.err.println("Tiempo total de ejecución: "+tiempo+" nanosegundos \n");
+    System.err.println("Comparaciones: "+comparaciones+"\n");
+    System.out.println("Intercambios: "+cambios+"\n");
+}
+
+private void mergeSortRec(Candidato[] arr, int l, int r, int atributo) {
+    if (l < r) {
+        int m = (l+r)/2;
+        mergeSortRec(arr, l, m, atributo);
+        mergeSortRec(arr, m+1, r, atributo);
+        merge(arr, l, m, r, atributo);
     }
+}
+
+private void merge(Candidato[] arr, int l, int m, int r, int atributo) {
+    int n1 = m - l + 1;
+    int n2 = r - m;
+
+    Candidato[] L = new Candidato[n1];
+    Candidato[] R = new Candidato[n2];
+
+    for (int i = 0; i < n1; i++)
+        L[i] = arr[l+i];
+    for (int j = 0; j < n2; j++)
+        R[j] = arr[m+1+j];
+
+    int i = 0, j = 0, k = l;
+    while (i < n1 && j < n2) {
+        if (!mayorQue(L[i], R[j], atributo)) {
+            arr[k] = L[i];
+            i++;
+        } else {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+
+    while (i < n1) {
+        arr[k] = L[i];
+        i++; k++;
+    }
+    while (j < n2) {
+        arr[k] = R[j];
+        j++; k++;
+    }
+}
 }
--- Codigo/Main.java
+++ Codigo/Main.java
@@ -7,21 +7,74 @@ public class Main{
             System.out.println("Ingrese el tamaño de los eventos (m): \n");
             int m = sc.nextInt();
             Controlador c = new Controlador(n,m);
+
+            Candidato candidatos[] = c.getCandidatos();
+
+            System.out.println("Estado Inicial: \n 1) Invertido \n 2) Casi ordenado \n Cualquier otro numero) Aleatorio \n");
+            int estado = sc.nextInt();
+            System.out.println("Atributo Inicial: \n"); mostrarOpciones();
+            int atributoInicial = sc.nextInt();
+            while(atributoInicial<1 && atributoInicial>5){
+                    System.out.println("Ingrese un dato valido: \n");
+                    atributoInicial = sc.nextInt();
+            }
+            c.estado(candidatos, atributoInicial, estado);
             while(true){ 
-                System.out.println("1) Mostrar candidatos\n 2) Burbuja\n 3) Seleccion\n 4) Insercion\n 0) Salir\n");
+                System.out.println("1) Mostrar candidatos\n 2) Burbuja\n 3) Seleccion\n 4) Insercion\n 5) Merge \n 6) Quick \n 0) Salir");
                 int dec = sc.nextInt();
                 switch (dec) {
                     case 1:
                         c.mostrarCandidatos(n); 
                         break;
                     case 2:
-                        c.burbuja();
+                        mostrarOpciones();
+                        int algo = sc.nextInt();
+                        while(algo<1 && algo>5){
+                            System.out.println("Ingrese un dato valido: \n");
+                            algo = sc.nextInt();
+                        }
+                        c.burbuja(candidatos, algo);
+                        mostrarOrdenados(candidatos);
                         break;
                     case 3:
-                        c.seleccion();
+                        mostrarOpciones();
+                        algo = sc.nextInt();
+                        while(algo<1 && algo>5){
+                            System.out.println("Ingrese un dato valido: \n");
+                            algo = sc.nextInt();
+                        }
+                        c.seleccion(candidatos, algo);
+                        mostrarOrdenados(candidatos);
                         break;
                     case 4:
-                        c.insercion();
+                        mostrarOpciones();
+                        algo = sc.nextInt();
+                        while(algo<1 && algo>5){
+                            System.out.println("Ingrese un dato valido: \n");
+                            algo = sc.nextInt();
+                        }
+                        c.insercion(candidatos, algo);
+                        mostrarOrdenados(candidatos);
+                        break;
+                    case 5:
+                        mostrarOpciones();
+                        algo = sc.nextInt();
+                        while(algo<1 && algo>5){
+                            System.out.println("Ingrese un dato valido: \n");
+                            algo = sc.nextInt();
+                        }
+                        c.mergeSort(candidatos, algo);
+                        mostrarOrdenados(candidatos);
+                        break;
+                    case 6:
+                        mostrarOpciones();
+                        algo = sc.nextInt();
+                        while(algo<1 && algo>5){
+                            System.out.println("Ingrese un dato valido: \n");
+                            algo = sc.nextInt();
+                        }
+                        c.quickSort(candidatos, algo);
+                        mostrarOrdenados(candidatos);
                         break;
                     case 0:
                         System.out.println("Saliendo...");
@@ -34,5 +87,15 @@ public class Main{
         } catch (Exception e) {
             System.out.println("Error: "+e.getMessage());
         } 
+    }
+    public static void mostrarOpciones(){
+        System.err.println("1) Por Distancia \n 2) Por Horas \n 3) Por Prebendas \n 4) Por Politicos \n 5) Por Corrupcion \n");
+    }
+    public static void mostrarOrdenados(Candidato[] candidatos){
+        System.err.println("Candidatos Ordenados: \n");
+        for(Candidato c: candidatos){
+            System.err.println(c);
+            System.err.println("--------------------------------------------------");
+        }
     }
 }
